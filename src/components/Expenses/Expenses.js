2c8090d1c8b4ec75bcf6e5c2ede93e45852e453e
Diogo Araujo <diogo_araujo9@hotmail.com>
2021-10-10 20:33:10 -0300

    chore: organização de pastas

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,12 @@
 import "./Expenses.css";
+import Card from '../UI/Card';
 import ExpenseItem from './ExpenseItem';
 
-function Expenses(props) {
+const Expenses = (props) => {
   const expenses = props.expenses;
 
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
@@ -29,7 +30,7 @@ function Expenses(props) {
         amount={expenses[3].amount}
         date={expenses[3].date}
       ></ExpenseItem>
-    </div>    
+    </Card>    
   );
 }
 
