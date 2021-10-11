1e7c195800ba11214dff2c13598ae5978bb0e1b6
Diogo Araujo <diogo_araujo9@hotmail.com>
2021-10-11 00:38:58 -0300

    feat: formulário de criação de despesa e filtro por ano

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,24 +1,35 @@
+import { useState } from "react";
 import "./Expenses.css";
-import Card from '../UI/Card';
-import ExpenseItem from './ExpenseItem';
+import Card from "../UI/Card";
+import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
   const expenses = props.expenses;
+  const [filteredYear, setFielteredYear] = useState("2020");
+
+  const filterChangeHandler = (filter) => {
+    setFielteredYear(filter);
+  };
 
   return (
     <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onFilterChange={filterChangeHandler}
+      />
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
         date={expenses[0].date}
       ></ExpenseItem>
-  
+
       <ExpenseItem
         title={expenses[1].title}
         amount={expenses[1].amount}
         date={expenses[1].date}
       ></ExpenseItem>
-  
+
       <ExpenseItem
         title={expenses[2].title}
         amount={expenses[2].amount}
@@ -30,8 +41,8 @@ const Expenses = (props) => {
         amount={expenses[3].amount}
         date={expenses[3].date}
       ></ExpenseItem>
-    </Card>    
+    </Card>
   );
-}
+};
 
 export default Expenses;
